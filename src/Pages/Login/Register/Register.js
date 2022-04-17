c36f4c75415701773ee77e51b1ede0084cfba753
AbidHasan0 <abidhasanrazvi@gmail.com>
2022-04-18 02:37:08 +0600

    add to toast

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useCreateUserWithEmailAndPassword, useUpdateProfile } from 'react-firebase-hooks/auth';
 import './Register.css'
 import auth from '../../../firebase.init'
 import SocialLogin from '../SocialLogin/SocialLogin';
+import Loading from '../../Shared/Loading/Loading';
 
 const Register = () => {
    const [agree, setAgree] = useState(false);
@@ -13,24 +14,34 @@ const Register = () => {
       loading,
       error,
    ] = useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
+   const [updateProfile, updating, updateError] = useUpdateProfile(auth);
 
    const navigate = useNavigate();
+
+   if (loading || updating) {
+      return <Loading></Loading>
+
+   }
 
 
    const navigateLogin = () => {
       navigate('/login')
    }
-   if (user) {
-      navigate('/home')
-   }
 
-   const handleRegister = (event) => {
+   // if (user) {
+   //    navigate('/home')
+   // }
+
+   const handleRegister = async (event) => {
       event.preventDefault();
       const name = event.target.name.value;
       const email = event.target.email.value;
       const password = event.target.password.value;
       if (agree) {
-         createUserWithEmailAndPassword(email, password);
+         await createUserWithEmailAndPassword(email, password);
+         await updateProfile({ displayName: name });
+         alert('Updated profile');
+         navigate('/home')
       }
 
    }

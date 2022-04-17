83682bea1e28334ea858e51c6fc49f3a401726a5
AbidHasan0 <abidhasanrazvi@gmail.com>
2022-04-17 15:15:23 +0600

    add to login form

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 const Register = () => {
    return (
-      <div>
+      <div className='register-form'>
          <h1>Please Register</h1>
+         <form>
+            <input type="text" name="name" id="" placeholder='Your Name' />
+
+            <input type="email" name="email" id="" placeholder='Your Mail' required />
+
+            <input type="password" name="password" id="" placeholder='Your Password' required />
+
+            <input type="submit" value="Register" />
+         </form>
       </div>
    );
 };

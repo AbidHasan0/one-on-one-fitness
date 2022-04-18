261fa895ab1149847b2c7d3164acb77e4768f562
AbidHasan0 <abidhasanrazvi@gmail.com>
2022-04-18 15:58:26 +0600

    add to chaking form on booking

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -1,9 +1,29 @@
 import React from 'react';
 
 const Checkout = () => {
+
+
+   const handleSubmit = (event) => {
+      event.preventDefault();
+      alert('Sucessfull your booking')
+      handleSubmit.value = "";
+   }
    return (
-      <div>
-         <h1>Please Checkout Your Booking</h1>
+      <div className='py-4'>
+         <div className='register-form'>
+            <h2 style={{ textAlign: 'center' }} className="text-primary">Please Booking</h2>
+            <form onSubmit={handleSubmit}>
+               <input type="text" name="name" id="" placeholder='Your Name' required />
+
+               <input type="number" name="number" id="" placeholder='Your Number' required />
+
+               <input type="Address" name="Address" id="" placeholder='Your Address' required />
+
+               <input
+                  className='w-50 mx-auto btn btn-primary mt-2'
+                  type="submit" value="submit" />
+            </form>
+         </div>
       </div>
    );
 };
